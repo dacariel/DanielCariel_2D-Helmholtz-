0768dc5aef558a7b97ea2c55b4119ba8f2fe1870
dacariel <dacariel@uh.edu>
2017-05-04 08:23:28 -0500

    Code Annotations

--- SRC/Helmholtz_2D.m
+++ SRC/Helmholtz_2D.m
@@ -1,67 +1,64 @@
 %   2D Helmholtz Equation
 %   Daniel Cariel
-%       This codes..... Add description 
+%       This code 
 
 
-%Region for the Helmholtz Equation
+%SURFACE REGION FOR HELMHOLTZ EQUATION 
 clc; clear all;
-n=input('Input value of n:');
-a_x= -pi;
-b_x= pi;
-a_y= -pi;
-b_y= pi;
-x= linspace(a_x,b_y,n);
-y= linspace(a_y,b_y,n);
+n=input('Input value of n:'); %Mesh size for Gauss Seidel approximation. Number of nodes
+a_x= -pi; %Lower boundary in the x-axis
+b_x= pi;  %Upper boundary in the y-axis 
+a_y= -pi; %Lower boundary in the x-axis
+b_y= pi;  %Upper boundary in the y-axis
+x= linspace(a_x,b_y,n); %x vector describing nodes in the x-axis
+y= linspace(a_y,b_y,n); %y vector describing nodes in the y-axis
 
-%Discretization Coefficients
-   gamma= pi;
+%Discretization Coefficients and initial guess 
+gamma=0;      %Wave constant 
+h= (2*b_x)/n; %Discretization step
+u= zeros(n);  %Initial guess for gauss seidel approximation 
 
-   h= (2*b_x)/n;
+%%BOUNDARY CONDITIONS
+%Dirishlet:
+
+u(:,1)= b_y.*((b_y-a_y).^2)+ ((x(:)-a_x)./(b_x-a_x)).*(((b_y-a_y).^2).*cos(pi.*(b_y/a_y))-b_y.*(b_y-a_y).^2); %Boundary condition at the left edge of the prescribed region
+
+u(n,:)= ((y(:)-a_y).^2).*cos(pi.*(y(:)/a_y)); %Boundary condition at the 
  
-%  GAUSS SEIDEL NUMERICAL SOLVER 
- 
- u= zeros(n); % Initial guess for gauss seidel approximation 
- 
- %Boundary Conditions
-    %Dirishlet 
+u(1,:)= y(:).*(y(:)-a_y).^2; %Boundary condition at the top edge of the prescribed region 
 
-    u(:,1)= b_y.*((b_y-a_y).^2)+ ((x(:)-a_x)./(b_x-a_x)).*(((b_y-a_y).^2).*cos(pi.*(b_y/a_y))-b_y.*(b_y-a_y).^2);
+%GAUSS SEIDEL ITERATIONS
+F=zeros(n); %Initia matrix for the forcing function of in the Helmholtz Equation 
+error=1; %Initial value of the error for the Gauss Seidel numerical solver
+iteration=0; %Iteration counter intended to keep track of the convergence rate of the code 
 
-    u(n,:)= ((y(:)-a_y).^2).*cos(pi.*(y(:)/a_y));
- 
-    u(1,:)= y(:).*(y(:)-a_y).^2;
-
- %Gauss Seidel Iterations
-
- error=1; u
- iteration=0;
-
-% while max(error(:))>=1e-6
-while iteration < 5000
-    
-   
-  iteration=iteration+1;
-  u_0=u;
+while max(error(:))>=1e-6   %Gauss Seidel iterations will continue until the error is within the desired tolerance 1e-6
+  iteration=iteration+1;    %Iteration step
+  u_0=u;                    %Solution of u from previous iteration. It is used to calculate the error
     for i=2:n-1
         for j=2:n-1
-          F(i,j)= cos((pi/2)*(2*((x(i)-a_x)/(b_x-a_x))+1))*sin((pi*y(j)-a_y)/(b_y-a_y));
-          u(i,j)= (1/((gamma*h^2)-4))*((h^2)*F(i,j)-(u(i+1,j)+u(i-1,j)+u(i,j+1)+u(i,j-1)));
+          F(i,j)= cos((pi/2)*(2*((x(i)-a_x)/(b_x-a_x))+1))*sin((pi*y(j)-a_y)/(b_y-a_y));    %Forcing function describing the problem 
+          u(i,j)= (1/((gamma*h^2)-4))*((h^2)*F(i,j)-(u(i+1,j)+u(i-1,j)+u(i,j+1)+u(i,j-1))); % Discretization solution of the 2D Helmholtz equation
         end 
-        u(i,n)= (1/((gamma*h^2)-4))*((h^2)*F(i,j)-(u(i+1,j)+u(i-1,j)+u(i,j+1)+u(i,j+1))); 
+        u(i,n)= (1/((gamma*h^2)-4))*((h^2)*F(i,j)-(u(i+1,j)+u(i-1,j)+u(i,j+1)+u(i,j+1)));   %Neumann boundary condition applied to the bottom edge of the prescribed region 
     end
-     u_f=u;
-
-      error= abs((u_f-u_0)./(u_f))
-
+     u_f=u;                             %Solution of u from most recent iteration. It is compared with the previous iteration to calculate the error
+     error= abs((u_f-u_0)./(u_f));      %Error calculation. The error drives the Gauss Seidel solver until tolerance is reached
 end 
 
 
-iteration
-
-figure(1)
-surf(x,y,u)
-figure(2)
-contourf(u)
-
-     
-
+%PLOT OF THE RESULTS
+% 2D contour of the surface
+figure      
+contourf(u) 
+colorbar('location','eastoutside','fontSize',11);
+xlabel('X Number of Nodes in X-direction','fontSize',11); %3D figure lable on the x axis
+ylabel('Y Number of Nodes in Y-direction','fontSize',11); %3D figure lable on the y axis
+title('Gauss Seidel for Helmhotlz') 
+% 3D surface graph of the solution 
+figure
+surf(x,y,u,'EdgeColor','none')
+xlabel('X Number of Nodes in X-direction','fontSize',11); %3D figure lable on the x axis
+ylabel('Y Number of Nodes in Y-direction','fontSize',11); %3D figure lable on the y axis
+zlabel('Position U','fontSize',12);                       %3D figure lable on the z axis
+title('2D Helmholtz Solution -  Gauss Seidel Numerical Solver');
