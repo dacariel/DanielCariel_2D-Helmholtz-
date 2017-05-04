72b66705aeec57872b577c3de083feaa9a7d9cf8
dacariel <dacariel@uh.edu>
2017-05-03 22:40:28 -0500

    Trial 1 for convergence

--- SRC/Helmholtz_2D.m
+++ SRC/Helmholtz_2D.m
@@ -34,36 +34,39 @@ y= linspace(a_y,b_y,n);
     %Dirishlet 
     u(:,1)= b_y.*((b_y-a_y).^2)+ ((x(:)-a_x)/(b_x-a_x)).* (((b_y-a_y).^2).*cos(pi.*(b_y/a_y))-b_y.*(b_y-a_y).^2);
 
-    u(1,:)= ((y(:)-a_y).^2).*cos(pi.*(y(:)/a_y));
+    u(n,:)= ((y(:)-a_y).^2).*cos(pi.*(y(:)/a_y));
  
-    u(n,:)= y(:).*(y(:)-a_y).^2;
+    u(1,:)= y(:).*(y(:)-a_y).^2;
 
 
  
  %Gauss Seidel Iterations
  error=1;
  iteration=0;
-%  
-while error(:)>=1e-6
-% while iteration < 5
-    iteration=iteration+1;
-    u_0=u;
- for i=2:n-1
-     for j=2:n-1
+
+while max(max(error))>=1e-6
+% while iteration < 100
+   
+  iteration=iteration+1;
+  u_0=u;
+    for i=2:n-1
+        for j=2:n-1
           F(i,j)= cos((pi/2)*(2*((x(i)-a_x)/(b_x-a_x))+1))*sin((pi*y(j)-a_y)/(b_y-a_y));
           u(i,j)= (1/((gamma*h^2)-4))*((h^2)*F(i,j)-(u(i+1,j)+u(i-1,j)+u(i,j+1)+u(i,j-1)));
-     end 
-    u(i,n)= (1/((gamma*h^2)-4))*((h^2)*F(i,j)-(u(i+1,j)+u(i-1,j)+u(i,j+1)+u(i,j+1))); 
- end 
- u_f=u;
-     error= abs (u_f.\(u_f-u_0));
-    
+        end 
+        u(i,n)= (1/((gamma*h^2)-4))*((h^2)*F(i,j)-(u(i+1,j)+u(i-1,j)+u(i,j+1)+u(i,j+1))); 
+    end
+     u_f=u;
+     error= abs(u_f-u_0)./(u_f)
+
 end 
+
+
 iteration
 
-
+figure(1)
 surf(x,y,u)
-figure
+figure(2)
 contourf(u)
      
  
